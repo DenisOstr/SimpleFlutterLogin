fac4dd681e987aeb73474a46fab03d8370599448
Denis Ostrovsky <denisostrovskis1@gmail.com>
2020-03-17 21:55:01 +0200

    added bottom wave

--- lib/Pages/loginPage.dart
+++ lib/Pages/loginPage.dart
@@ -101,7 +101,7 @@ class LoginPageState extends State<LoginPage> {
                         )
                       ],
                     ),
-                    SizedBox(height: 30.0),
+                    SizedBox(height: 20.0),
                     Center(
                       child: CustomElevation(
                         height: 65.0,
@@ -155,16 +155,16 @@ class LoginPageState extends State<LoginPage> {
             )
           ],
         ),
-        SizedBox(height: 10.0),
+        // SizedBox(height: 10.0),
         ClipPath(
           clipper: BottomWaveClipper(),
           child: Container(
-            height: MediaQuery.of(context).size.height / 2,
+            height: 80.0,
             decoration: BoxDecoration(
               gradient: LinearGradient(
-                colors: orangeGradients,
+                colors: purpleGradients,
                 begin: Alignment.topLeft,
-                end: Alignment.center,
+                end: Alignment.bottomCenter,
               )
             ),
           ),
@@ -174,10 +174,10 @@ class LoginPageState extends State<LoginPage> {
   }
 }
 
-const List<Color> orangeGradients = [
-  Color(0xFFFF9844),
-  Color(0xFFFE8853),
-  Color(0xFFFD7267),
+const List<Color> purpleGradients = [
+  Color(0xFFA454C7),
+  Color(0xFFDB61BB),
+  Color(0xFFE865B8),
 ];
 
 class BottomWaveClipper extends CustomClipper<Path> {
@@ -185,29 +185,16 @@ class BottomWaveClipper extends CustomClipper<Path> {
   Path getClip(Size size) {
     var path = Path();
 
-    var w = size.width;
-    var h = size.height;
+    var width = size.width;
+    var height = size.height;
 
-    path.lineTo(w, h);
+    path.moveTo(0.0, 0.0);
+    
+    path.cubicTo(80.0, 100.0, 200.0, 20.0, 320.0, height);
+    
+    path.lineTo(width, height);
 
-    var firstControlPoint = new Offset(0.0, 0.0);
-    var firstEndPoint = new Offset(size.width / 7, size.height / 2);
-
-    // path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy, firstEndPoint.dx, firstEndPoint.dy);
-    path.quadraticBezierTo(firstEndPoint.dx, firstEndPoint.dy, firstControlPoint.dx, firstControlPoint.dy);
-
-    var secondControlPoint = Offset(size.width / 7, size.height / 5);
-    var secondEndPoint = Offset(size.width, size.height);
-
-    // path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy, secondEndPoint.dx, secondEndPoint.dy);
-    path.quadraticBezierTo(secondEndPoint.dx, secondEndPoint.dy, secondControlPoint.dx, secondControlPoint.dy);
-
-    // var thirdControlPoint = Offset(size.width, 0);
-    // var thirdEndPoint = Offset(size.width, size.height);
-
-    // path.quadraticBezierTo(thirdControlPoint.dx, thirdControlPoint.dy, thirdEndPoint.dx, thirdEndPoint.dy);
-
-    path.lineTo(0.0, h);
+    path.lineTo(0.0, height);
 
     path.close();
     return path;
